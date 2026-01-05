9e37f39c6b54eb567a358ae15fa9f7f23c073e05
NightDev19 <sherwintajan143@gmail.com>
2026-01-05 15:53:08 +0800

    Basic of MATLAB

---
+++ Arrays/array.m
@@ -0,0 +1,16 @@
+% Basic Array
+arr = [1 2 3 4]
+% 3D Array
+arr3D = [
+1 2 3
+4 5 6
+7 8 9
+]
+% Array with 1 Column
+z = zeros(5,1)
+
+% Matrix and Array Operation
+
+arr3D + 10
+
+sin(arr3D)
---
+++ sample.m
@@ -0,0 +1,14 @@
+% Sample basic Integer in MATLAB
+
+a = 1;
+b = 2; 
+
+% Basic Math Operation
+
+add = a + b; 
+diff = a - b;
+qou = a * b;
+div = a / b;
+expo = a ^ b + 1;
+disp("This is all of the Basics Math in MATLAB")
+disp(expo)
