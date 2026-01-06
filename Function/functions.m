2aca9c2bab720661132e6b7cd071e9c34ae7a7e2
NightDev19 <sherwintajan143@gmail.com>
2026-01-06 16:31:21 +0800

    Feat(Function) : Basic Calling of Built In Function

---
+++ Function/functions.m
@@ -0,0 +1,14 @@
+% Functions in MatLab
+%{
+Functions is a Callable method that can be used in different ways 
+
+%}
+arrList1 = [1 2 3 4];
+disp("Max Number on the list");
+disp(max(arrList1)); % Max is a function that gets the maximum number or data on the list of Number
+
+% Now Lets Create a Union
+arrList2 = [5 6 7 8];
+disp("Shows all the numbers in union list");
+unionList = union(arrList1,arrList2);
+disp(unionList);
