7d14f1509a00183bc412504c04840b32131f827c
NightDev19 <sherwintajan143@gmail.com>
2026-01-24 08:35:04 +0800

    Adding Linear Equation on Simulink with MATLAB

--- Project2_Run.m
+++ Project2_Run.m
@@ -1,5 +1,5 @@
 F = 2;
 A = 3;
-Ti = 0.5;
+Ti = 6;
 
 sim("Simulations/Intro_To_Simulink/Project2_2")
---
+++ linear_equation.m
@@ -0,0 +1,5 @@
+x = 3;
+x1 = 2;
+x2 = 4;
+x3 = 10;
+sim("Simulations/Intro_To_Simulink/linear_equation");
