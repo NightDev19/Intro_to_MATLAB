7e259b2889bc81892d94ddf86c7a115d6accde61
NightDev19 <sherwintajan143@gmail.com>
2026-02-03 16:22:17 +0800

    Add new Project using PID using Damper System

---
+++ DamplerSystemWithPIDScript.m
@@ -0,0 +1,29 @@
+%% Simulation 
+Sim_Time = 7;
+Step_Val = 1;
+M = 1;
+b = 10;
+k = 20;
+
+% Defining PID Control Parameter
+Kp = 350;
+Ki = 300;
+Kd = 50;
+
+% Capture simulation output explicitly
+out = sim("Intro_to_MATLAB/Simulations/Intro_To_Simulink/Dampler_System_with_PID");
+
+% Extract signals (formerly ans.IN / ans.OUT)
+IN  = out.IN;
+OUT = out.OUT;
+
+% Plotting Section
+figure;
+plot(IN.Time, IN.Data);
+hold on;
+plot(OUT.Time, OUT.Data);
+hold off;
+legend("IN","OUT");
+xlabel("Time (s)");
+ylabel("Signal");
+grid on;
