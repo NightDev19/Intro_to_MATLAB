53e90b024354e521e02cbb286d20f210e83404fc
NightDev19 <sherwintajan143@gmail.com>
2026-02-04 15:47:24 +0800

    Add a DC Motor Model

---
+++ Simulations/Day10/DC_Motor_Params.m
@@ -0,0 +1,9 @@
+% DC Motor
+V = 1; % Voltage
+J = 3.2284E-6; % kg.m^s
+b = 3.5077E-6;%Nms
+Kb = 0.0247; % V/rad/sec
+Kt = 0.0247; % Nm/Amp
+R = 0.4; %ohms
+L = 2.75E-6; %H
+sim("Intro_to_MATLAB/Simulations/Day10/DC_Motor")
